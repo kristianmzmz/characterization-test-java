f6ad3cd9f1821ca638d32be8a370e1f84871a6fd
Kristian Muñoz <kristian.mzmz@gmail.com>
2023-10-09 15:42:00 +0200

    Add a piece of code that is suitable to be tested using characterization tests

---
+++ characterization-test-java/src/test/kata/MainTest.java
@@ -0,0 +1,13 @@
+package kata;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void formatText() {
+        assertEquals("expected", "actual");
+    }
+}
