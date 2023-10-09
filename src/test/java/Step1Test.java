7c6f1c261c2eb67e86ff88adf2c233e6181d7321
Kristian Muñoz <kristian.mzmz@gmail.com>
2023-10-09 16:41:11 +0200

    Create some tests based on the characterization tests technique, first step: What happens with a string?

---
+++ src/test/java/Step1Test.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Step1Test {
+
+    @Test
+    public void shouldDoSomethingWithStrings() {
+        // Given
+        String aRandomString = "a random string";
+
+        // When
+        String aFormattedText = Main.formatText(aRandomString);
+
+        // Then
+        assertEquals("I don't know what I'm doing", aFormattedText);
+    }
+}
